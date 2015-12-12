142975079a5efafa373867e99b76c72ace120e0b
Yonas Berhe <yonas.berhe007@gmail.com>
2015-12-11 21:23:17 -0800

    init commit/done

---
+++ Assets/playerscript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// class must be the same as the fileanme
+public class playerscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizontal = Input.GetAxis ("Horizontal"); 
+		float vertical = Input.GetAxis ("Vertical");
+		transform.Translate (horizontal, vertical, 0);
+	}
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Did a tutorial to get introduced to Unity as a tool.
+
+
+Linke to tutorial: http://code.tutsplus.com/tutorials/introduction-to-unity3d--mobile-10752
+
+
+
